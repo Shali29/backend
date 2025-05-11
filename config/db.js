099ff8e2732051414c7474560abaf98c6dee312c
Shali29 <shalikaramanayaka21@gmail.com>
2025-05-11 22:42:46 +0530

    db modification

--- config/db.js
+++ config/db.js
@@ -1,25 +1,26 @@
+
 import sql from 'mssql';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const config = {
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    server: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    port: parseInt(process.env.DB_PORT),
-    options: {
-        encrypt: true,
-        trustServerCertificate: false,
-    },
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  server: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  port: parseInt(process.env.DB_PORT),
+  options: {
+    encrypt: true,
+    trustServerCertificate: true, // usually true for development
+  },
 };
 
 const pool = new sql.ConnectionPool(config);
 const poolConnect = pool.connect();
 
 export default {
-    sql,
-    pool,
-    poolConnect,
+  sql,
+  pool,
+  poolConnect,
 };
